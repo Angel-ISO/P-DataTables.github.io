3098cc5cb3e65fbd585eff3a9f5f5f859ad97c89
Angel-ISO <angelgabrielorteg@gmail.com>
2023-06-13 08:36:24 -0500

    feat(plantilla postgresql): :art: lista de paises y id respectivos
    id respectivo con pais recpectivo
    
    data tables y agrupacion con  bases de datos respectivas

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'desarrollo/inventario-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '64c280e8ec6e746bb5db64978574ab8c4e3a834f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'desarrollo/inventario-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '64c280e8ec6e746bb5db64978574ab8c4e3a834f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
